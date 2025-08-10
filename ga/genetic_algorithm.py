7a7f3ade31605f32c389591229fa1fca28a00537
teofeel <teodor.perun@gmail.com>
2025-08-10 20:14:51 +0200

    first

---
+++ ga/genetic_algorithm.py
@@ -0,0 +1,32 @@
+# generate population
+
+
+# fitness function
+
+
+# select parents function
+# pick two best if elitism choosen
+# create tourmnament 
+
+
+# crossover function
+# choose a random value which is less that lenght of dataset
+# add parent1's genes up to that point to the new chromosome
+# add parent2's genes from that point to the new chromosome
+# offspring is created
+
+
+# mutation function
+# mutate only offsprings
+# theres two ways to choose how many genes to mutate:
+    # choose 1-2 genes of offspring and then move to next one
+    # assign a random probability to every gene that determines if the gene will be mutated. Roll a dice for every gene and mutate it if lands within the probability
+
+
+# criteria satisfied function
+# criteria is satisfied when:
+    # if new population score is simillar to the last ones after x number of repetitions
+    # if the number of generation is reached
+    # if has optained optimal  
+
+
---
+++ main.py
@@ -0,0 +1,15 @@
+
+# load data
+def load_data(name):
+    pass
+
+# generate initial population
+# population is consisted of 2+ chromosomes
+# number of genes in chromosome is equal to length of dataset
+# chromosome is consisted of randomly selected items from dataset whose total weight can fit in backpack
+# max population size is 2^n (where n is length of dataset)
+def generate_initial_population(data, population_size, capcity):
+    pass
+
+if __name__ == '__main__':
+    pass
